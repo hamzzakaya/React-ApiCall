82213c6e1434a0e45e7a3380d337672632da80fb
hamzzakaya <hamzakaya4343@gmail.com>
2023-11-04 17:18:15 +0300

    header

---
+++ search-image/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchHeader from './SearchHeader';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <SearchHeader/>
+
+    </div>
+  );
+}
+
+export default App;
